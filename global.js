390787221da30b0285d59a84e0d51735f04982be
ivan <ivan@mailoo.org>
2016-03-04 23:41:29 +0100

    delete unused ping.txt

--- global.js
+++ global.js
@@ -1,75 +1,22 @@
+// load sounds
 var clic = new Array();
 for (var i = 0; i < 11; i++) { clic[i] = new buzz.sound( "sounds/clic"+i, { formats: [ "ogg", "mp3" ], preload: true, }); };
 var sonar = new buzz.sound( "sounds/sonar" , { formats: [ "ogg", "mp3" ] });
 
-$(document).ready(function () {
-
-	$(".border").hover(function(){
-		$(".border").css({"border-color":"#ddd"});
-	},function(){
-		$(".border").css({"border-color":"transparent"});
-	})
-	.click(function(){
-		$("#docu").toggle();
-		$("#mapButton").toggle();
-	});
-
-	// TOGGLE-CLICK implementation
-    $.fn.toggleClick = function(){
-	    var methods = arguments, // store the passed arguments for future reference
-	    count = methods.length; // cache the number of methods 
-	    //use return this to maintain jQuery chainability
-	    return this.each(function(i, item){
-	        var index = 0; // create a local counter for that element
-	        $(item).click(function(){ // bind a click handler to that element
-	            return methods[index++ % count].apply(this,arguments); // that when called will apply the 'index'th method to that element
-	            // the index % count means that we constrain our iterator between 0 and (count-1)
-	        });
-	    });
-	};
-	$('#mapButton').toggleClick (function(){
-			$(this).html("Country Name").css({ "border-left-style":"hidden", "border-right-style":"solid", "text-align":"right" });
-
-			for (var i = 0; i < group.children.length-1; i++) {
-				if ( typeof numberTexture[ group.children[i].name ] !== 'undefined' ){
-					group.children[i].material.map = numberTexture[ group.children[i].name ] ;
-					group.children[i].material.visible = true ;
-					group.children[i].material.needsUpdate = true;
-				}
-			}
-	    },function(){			        
-			$(this).html("Ping Timing").css({ "border-right-style":"hidden", "border-left-style":"solid", "text-align":"left" });
-			for (var i = 0; i < group.children.length-1; i++) {
-				group.children[i].material.map = nameTexture[i] ;
-				group.children[i].material.visible = true ;
-				group.children[i].material.needsUpdate = true;
-			}
-	});
-	$(window).load( function(){ setup(); });
-});
-
-
 	var names = ["AFGHANISTAN","ALBANIA","ALGERIA","ANDORRA","ANGOLA","ANTIGUA","ARGENTINA","ARMENIA","AUSTRALIA","AUSTRIA","AZERBAIJAN","BAHAMAS","BAHRAIN","BANGLADESH","BARBADOS","BELARUS","BELGIUM","BELIZE","BENIN","BHUTAN","BOLIVIA","BOSNIA","BOTSWANA","BRAZIL","BRUNEI","BULGARIA","BURKINA FASO","BURUNDI","CAMBODIA","CAMEROON","CANADA","CAPE VERDE","CENT. AFRICAN REP.","CHAD","CHILE","CHINA","CHYPRE","COLOMBIA","COMOROS","CONGO","COSTA RICA","CROATIA","CUBA","CZECH REP.","DENMARK","DJIBOUTI","DOMINICAN REP.","DOMINICA","EAST TIMOR","EGYPT","EQUATORIAL GUINEA","EQUATOR","ERITREA","ESTONIA","ETHIOPIA","FIJI","FILIPINOS","FINLAND","FRANCE","GABON","GAMBIA","GEORGIA","GERMANY","GHANA","GRANADA","GREECE","GUATEMALA","GUINEA BISSAU","GUINEA","GUYANA","HAITI","HONDURAS","HUNGARY","ICELAND","INDIA","INDONESIA","IRAN","IRAQ","IRELAND","ISRAEL","ITALY","IVORY COAST","JAMAICA","JAPAN","JORDAN","KAZAKHSTAN","KENYA","KIRIBATI","KUWAIT","KYRGYZSTAN","LAOS","LATVIA","LEBANON","LESOTHO","LIBERIA","LIBYA","LIECHTENSTEIN","LITHUANIA","LUXEMBOURG","MACEDONIA","MADAGASCAR","MALAWI","MALAYSIA","MALDIVES","MALI","MALTA","MARSHALL ISLANDS","MAURITANIA","MAURITIUS","MEXICO","MICRONESIA","MOLDAVIA","MONACO","MONGOLIA","MONTENEGRO","MOROCCO","MOZAMBIQUE","MYANMAR","NAMIBIA","NAURU","NEPAL","NETHERLANDS","NEW ZELAND","NICARAGUA","NIGERIA","NIGER","NORTH KOREA","NORWAY","OMAN","PAKISTAN","PALAOS","PANAMA","PAPUA","PARAGUAY","PERU","POLAND","PORTUGAL","QATAR","ROMANIA","RUSSIA","RWANDA","SALVADOR","SAMOA","SAO TOME & PRINCIPE","SAUDI ARABIA","SENEGAL","SERBIA","SEYCHELLES","SIERRA LEONE","SINGAPORE","SLOVAKIA","SLOVENIA","SOLOMON ISLANDS","SOMALIA","SOUTH AFRICA","SOUTH KOREA","SOUTH SUDAN","SPAIN","SRI LANKA","ST KITTS & NEVIS","ST LUCIE","ST MARTIN","ST VINCENT","SUDAN","SURINAME","SWAZILAND","SWEDEN","SWISS","SYRIA","TAJIKISTAN","TANZANIA","THAILAND","TOGO","TONGA","TRINIDAD & TOBAGO","TUNISIA","TURKEY","TURKMENISTAN","TUVALU","U.S.A.","UGANDA","UKRAINE","UNITED ARAB EM.","UNITED KINGDOM","URUGUAY","UZBEKISTAN","VANUATU","VENEZUELA","VIETNAM","YEMEN","ZAIRE","ZAMBIA","ZIMBABWE","LOCALHOST"];
 	var mouseX = 0, mouseY = 0,
 	camera, scene, renderer, group, groupLines, groupLinesCenter, controls, control=false, rotation=0, speed=0.001 ;
-
 	var imgs = new Array(194); 
 	var nameTexture = new Array(194);
-	var imgNumber = new Array(11); 
-	var numberTexture = new Array(1000);
 	for (var i = 0; i < imgs.length; i++) {
 		imgs[i] = new Image();
 		imgs[i].src = "countries/"+i+".png";
 	}			
-	for (var i = 0; i < imgNumber.length; i++) {
-		imgNumber[i] = new Image();
-		imgNumber[i].src = "numbers_0-9/"+i+".png";
-	}
-
-
 	var lines, line, img;
-	var labelSize = 0.5;
-	function setup() {
+
+
+	function setup() {  // setup 3D objects
+
 		var container, particles, particle;
 		container = document.createElement('div');
 		document.body.appendChild(container);
@@ -86,16 +33,10 @@ $(document).ready(function () {
 		scene.rotation.x = Math.PI/2;
 		scene.rotation.y = 0;
 
-		// stats
-		stats = new Stats();
-		stats.domElement.style.position = 'absolute';
-		stats.domElement.style.top = '50px';
-		stats.domElement.style.right = '50px';
-		container.appendChild( stats.domElement );
+		blackPlane();
 
 		group = new THREE.Group();
 		groupLinesCenter = new THREE.Group();
-		for (var i = 0; i < numberTexture.length; i++) { setupNumber(i); }  // setup number labels
 		for (var i = 0; i < 194; i++) {
 			var sprite = makeTextSprite( names[i], i ); // setup country name labels
 			group.add( sprite );
@@ -126,59 +67,42 @@ $(document).ready(function () {
 		document.addEventListener( 'touchmove', onDocumentTouchMove, false );
 		document.addEventListener( "mousedown", function(){ control = true; });
 		document.addEventListener( "mouseup"  , function(){ control = false; });
-		//$(document).load( function(){ 
-		//window.onload = function(){ 
-			draw(); console.log("windowLoaded"); 
-				
-		//});
+
+		draw();
 	}
 
 	function makeTextSprite( message,i ){
 		var canvas = document.createElement('canvas');
 		var context = canvas.getContext('2d');
 		var img = imgs[i];
-		//context.canvas.width  = 300 ;  context.canvas.height = 50 ;    // maybe usless ...
 		context.drawImage(img, context.canvas.width/2-img.width/2,  context.canvas.height/2-img.height/2 );
 
 		nameTexture[i] = new THREE.Texture( canvas ); nameTexture[i].needsUpdate = true;
 
 		var spriteMaterial = new THREE.SpriteMaterial({ map: nameTexture[i], name: i, opacity: 0 } );
 		var sprite = new THREE.Sprite( spriteMaterial );
-		sprite.scale.set(context.canvas.width/2.1,context.canvas.height/2.1 ,1.0);  // labels size
+
+		var x = 3000/window.innerWidth; // labels size
+		if ( window.innerWidth < 800 ) x = 3 ;
+		sprite.scale.set(context.canvas.width/x,context.canvas.height/x ,1.0);
 		return sprite;	
-	}
-
-	function setupNumber(i){
-			var canvas = document.createElement('canvas');
-			var context = canvas.getContext('2d');
-			
-			if (i>999)
-			context.drawImage(imgNumber[Math.floor(i/1000)%10], context.canvas.width/2-16-16, context.canvas.height/2-20);// 1000
-			if (i>99)
-			context.drawImage(imgNumber[Math.floor(i/100)%10], context.canvas.width/2-16-16, context.canvas.height/2-20 );// 100
-			context.drawImage(imgNumber[Math.floor(i/10)%10], context.canvas.width/2-16   , context.canvas.height/2-20 );// 10
-			context.drawImage(imgNumber[i%10],				 context.canvas.width/2,      context.canvas.height/2-20 );     // 0-9
-			context.drawImage(imgNumber[10],				context.canvas.width/2+19,  context.canvas.height/2-20 );  // MS
-
-	        numberTexture[i] = new THREE.Texture( canvas ); numberTexture[i].needsUpdate = true ;
 	}
 
 	function displayLinesCenter(i, x, y, z){
 		if ( typeof groupLinesCenter !== 'undefined' && groupLinesCenter.children.length > i ){
 			groupLinesCenter.children[i].geometry.vertices[1].set(x,y,z) ;  
 	      	groupLinesCenter.children[i].visible = true ;
-	      	setTimeout( function(){ groupLinesCenter.children[i].visible = false; }, 200) ;  
+	      	setTimeout( function(){ groupLinesCenter.children[i].visible = false; }, 200) ;  // isVisible duration in millisecond
 		}
 	}
 
 	function draw() {
-		requestAnimationFrame( draw );
-		stats.update();
 
 		if (!control) rotation -= speed;
 		group.rotation.z = rotation; 
 		groupLines.rotation.z = rotation +0.001;
 		groupLinesCenter.rotation.z = rotation +0.001;
+
 		for (var i = 0; i < groupLines.children.length; i++) {  // triangulationLines opacity
 			var off = groupLines.children[i] ;
 	    	var vector = off.geometry.vertices[0].clone();
@@ -189,13 +113,14 @@ $(document).ready(function () {
 		};
 
 		renderer.render( scene, camera );
+		requestAnimationFrame( draw );
 	}
 	function onWindowResize() {
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
 		renderer.setSize( window.innerWidth, window.innerHeight );
 	}
-	function onDocumentMouseMove(event) {
+	function onDocumentMouseMove( event ) {
 		if (control) rotation += (mouseX - event.clientX)/200;
 		mouseX = event.clientX;
 		mouseY = event.clientY;
@@ -217,3 +142,26 @@ $(document).ready(function () {
 
 	Number.prototype.map = function(a,b,c,d){return c+(d-c)*((this-a)/(b-a))};
 
+
+	// transparent vertical black plane to shadow background labels
+	function blackPlane(){
+		var objects = new THREE.Object3D();
+		Allobjects = new THREE.Object3D();
+		Allobjects.add(objects);
+		scene.add(Allobjects);
+		var materialP = new THREE.MeshLambertMaterial({
+		    color: 0xFF0000,
+		    opacity:0.2,
+		    overdraw: true,
+		    side: THREE.DoubleSide
+		});
+		var imgplane = new THREE.Mesh(new THREE.PlaneGeometry(1000, 1000),materialP);
+		imgplane.renderDepth = 1;
+		// plane position
+		objects.add(imgplane);
+		var angleRadian = Math.PI * Math.round(90) / 180;
+		imgplane.rotation.x = angleRadian;
+		imgplane.position.x = 0;
+		imgplane.position.z = 0;
+		imgplane.position.y = -50;
+	}
--- networkjs.pde
+++ networkjs.pde
@@ -1,47 +1,45 @@
-int[] txtLength = { 147, 88, 87, 100, 82, 92, 120, 97, 116, 91, 131, 103, 94, 139, 111, 97, 96, 72, 66, 88, 81, 78, 117, 74, 81, 104, 153, 98, 109, 119, 83, 128, 212, 57, 62, 66, 85, 108, 105, 72, 122, 89, 57, 124, 105, 99, 175, 105, 127, 67, 215, 99, 89, 90, 97, 40, 103, 90, 83, 71, 81, 90, 104, 72, 98, 82, 127, 162, 79, 85, 56, 118, 103, 89, 57, 114, 51, 50, 90, 74, 58, 136, 94, 70, 86, 139, 69, 90, 83, 138, 53, 72, 98, 96, 81, 59, 167, 113, 148, 124, 143, 85, 105, 107, 52, 71, 210, 133, 121, 82, 133, 107, 91, 110, 148, 105, 145, 110, 90, 76, 68, 155, 135, 121, 85, 65, 148, 91, 63, 104, 80, 89, 70, 111, 58, 83, 111, 69, 98, 79, 91, 109, 74, 234, 144, 95, 76, 134, 150, 120, 101, 103, 199, 93, 152, 147, 151, 64, 109, 185, 94, 116, 125, 73, 114, 122, 90, 69, 62, 124, 104, 104, 55, 70, 209, 86, 84, 170, 84, 64, 86, 94, 184, 183, 103, 133, 101, 123, 96, 75, 61, 80, 114, 122}; 
-int[] slider = {58 ,60 ,20 ,30 ,24 ,0 ,127 ,10};
-int t = 1, fps = 30, fpss = fps*15; // 15 secondes entre chaque reactualisation
-float rota = 0 ;
-PImage[] imgs = new PImage[194];
-PShape[] svgs = new PShape[195];
 String[] tmp, tri ;
 String[][] mapp ;
 float[][] points ;  
 int[][] links ;
+
+float  x,y,z, x1, y1, z1, x2, y2, z2 , fadeOut;
 float[][] ping = new float[194][15];
 float[] pingOld = new float[194];
-float control=2*PI, control2=-PI, 
-  controlTaille = (window.innerWidth*0.9 > window.innerHeight) ? window.innerHeight/6 : window.innerWidth/11;  // taille de la sphere
-float  x,y,z, x1, y1, z1, x2, y2, z2 , fadeOut;
 PVector[] vPos = new PVector[200] ;
 
+// globe responsive size
+float globSize = (window.innerWidth*0.9 > window.innerHeight) ? window.innerHeight/4.6 : window.innerWidth/11;  
+int t = 1, 
+fps = 30, 
+fpss = fps*15, // 15 seconds between each pingMap actualisation
+startFadeOut = 90, // start fadeout in second from actualisation
+fadeOutSpeed = 0.011; // fadeout speed in second 
 
 void setup() {
-
   size(1,1);
   noSmooth();
   frameRate(fps);
   dataRequest("savedPing.txt");
-  dataRequest("ping.txt");
+  dataRequest("doping/ping.txt");
   mapSetup();
 }
 
 void draw() {
-  if (frameCount%(fpss*10)==0) dataRequest("ping.txt");
-  if (frameCount%(fpss)==0) t++ ;  if(t==11) t = 1 ; rota += 0.002;
-  if (frameCount%fpss  > fpss-90 && frameCount%fpss < fpss) fadeOut -= 0.011 ;
+  if (frameCount%(fpss*10)==0) dataRequest("doping/ping.txt");
+  if (frameCount%(fpss)==0) t++ ;  if(t==11) t = 1 ; 
 
-  if ( typeof group !== 'undefined' && typeof numberTexture[50] !== 'undefined')
-    display(0);
+  // labels fadeout speed
+  if (frameCount%fpss > fpss-startFadeOut && frameCount%fpss < fpss) fadeOut -= fadeOutSpeed ;
+
+  if ( typeof group !== 'undefined' ) display();
+  displayText();
 }
 
 void display() {
 
-  float s = map(slider[1],0,127,0,1);
-
-
-  // SONAR
- // if(frameCount%(fpss) == int(fpss-3.9*fps) ) { sonar.stop().play(); }
+  // sonar sound
+  if(frameCount%(fpss) == int(fpss-3.9*fps) ) { sonar.stop().play(); }
 
     for(int i=0; i<links.length; i++){
       float coef1 = pos(links[i][0]);
@@ -57,7 +55,7 @@ void display() {
       if ( pos(links[i][0]) != 0  && pos(links[i][1]) != 0 ){
             groupLines.children[i].geometry.vertices[0].set(x1,y1,z1);
             groupLines.children[i].geometry.vertices[1].set(x2,y2,z2) ;
-            //groupLines.children[i].geometry.verticesNeedUpdate = true ;
+            groupLines.children[i].geometry.verticesNeedUpdate = true ;
       }
     }  
     for (int i = 0; i<tmp.length; i++){
@@ -69,18 +67,18 @@ void display() {
 
       if ( frameCount%fpss-1 == int( map(ping[i][t],0,12000,0,fpss*5) ) ){     
         int ran = floor(random(0,11));
-        //clic[ ran ].stop().play();
+        clic[ ran ].stop().play();
         displayLinesCenter(i, x, y, z);
 
 // LABELS NAMES
         group.children[i].position.set( x, y, z );
         group.children[i].material.opacity = 1 ;
         group.children[i].name =  int(ping[i][t]);
-        group.children[i].material.overdraw = true;  // affichage safari ?
+        //group.children[i].material.overdraw = true;  // affichage safari ?
       }
-      
-      if ( frameCount%fpss == fpss-90 ) fadeOut = 1;     //   FADEOUT LABELS
-      if ( frameCount%fpss  > fpss-90 && frameCount%fpss < fpss ){
+//   FADEOUT LABELS
+      if ( frameCount%fpss == fpss-startFadeOut ) fadeOut = 1;    
+      if ( frameCount%fpss  > fpss-startFadeOut && frameCount%fpss < fpss ){
         group.children[i].material.opacity = fadeOut ;
       }
       
@@ -89,17 +87,21 @@ void display() {
         pingOld[i] = ping[i][t];
     }
 }
+
 int timer(int index){
   int timer = 0;
   if (frameCount%fpss < map(ping[index][t],0,12000,0,fpss*5) )
     { timer = 1; }else{ timer = 2; }
   return timer;
 }
+
 float pos(int index){
+
   float r = 0;
-  if (timer(index)==1) { r = log(1+ pingOld[index]/slider[7] ) * controlTaille;  // r = (0.005 * pingOld[index] ) * controlTaille;
-  } else {               r = log(1+ ping[index][t]/slider[7] ) * controlTaille; }
-  r = constrain(r, slider[5]*10, slider[6]*10);
+  if (timer(index)==1) { r = ( log(1+ pingOld[index]/10 )-1.3  ) *globSize  ;  // r = (0.005 * pingOld[index] ) * globSize;
+  } else {               r = ( log(1+ ping[index][t]/10 )-1.3  ) *globSize  ; }
+  
+  r = constrain(r, 10, 430);
   return r;
 }
 void dataRequest(String dataPath) {
@@ -108,7 +110,6 @@ void dataRequest(String dataPath) {
     for (int i = 0; i<data.length; i++){  
       String[] temp = splitTokens( data[i], " ");
 
-        console.log(temp[0]);
       for (int j = 0; j<temp.length; j++){ 
         
         if( int(temp[0]) <= 193 && int(temp[j])>1 )
@@ -117,16 +118,18 @@ void dataRequest(String dataPath) {
     }
   }
 }
+// send ping time data to html DOM layer
 void displayText(){ 
   for (int i = 0; i<tmp.length; i++){
     if(timer(i)==2)
-      document.getElementById( mapp[i][1].toUpperCase() ).innerHTML= int(ping[i][t]) ;
+      document.getElementById( mapp[i][1].toUpperCase() ).innerHTML = int(ping[i][t]) ;
     if(timer(i)==1)
-      document.getElementById( mapp[i][1].toUpperCase() ).innerHTML= "_" ;
+      document.getElementById( mapp[i][1].toUpperCase() ).innerHTML = "_" ;
   }
 }
 
 void mapSetup(){
+  // load world-map countries location
   tmp = loadStrings("data/mapOrdered.txt");
   mapp =  new String[tmp.length][6];
 
@@ -136,11 +139,11 @@ void mapSetup(){
       mapp[i][j] = tmp2[j];
     }
   }
-
-
+  // 2D world-map to 3D
   points = new float[tmp.length][2];
   for (int i = 0; i<tmp.length; i++){
-     points[i][0] = float(mapp[i][2]); points[i][1] = -float(mapp[i][3]);
+     points[i][0] = float(mapp[i][2]); 
+     points[i][1] = -float(mapp[i][3]);
   }
   for (int i = 0; i<points.length; i++){
     vPos[i] = new PVector( cos( radians( points[i] [1] ) ) * cos(radians(points[i][0])) ,
@@ -148,6 +151,8 @@ void mapSetup(){
                            sin( radians( points[i] [0] ) )  );
   }
 
+
+  // load triangulation lines : links[id][x/y]
   tri = loadStrings("data/triangulation.txt");
   links = new int[ tri.length/2 ][2] ;
 
@@ -155,6 +160,4 @@ void mapSetup(){
     links[i][0] = int(tri[2*i]) ;
     links[i][1] = int(tri[2*i+1]) ;
   }
-}
-void keyPressed() {  
 }
